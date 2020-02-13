75496b32cc2fc40be3ac404c92502c198e3e270d
Nickolas "Nick" Manes <59900634+manesn@users.noreply.github.com>
2020-02-12 23:08:53 -0500

    Finished.
    What's next? :100:

--- Scripts/Ship.gd
+++ Scripts/Ship.gd
@@ -5,4 +5,11 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("ui_left"):
+		position.x = position.x - 10
+	if Input.is_action_pressed("ui_right"):
+		position.x = position.x - -10
+	if Input.is_action_pressed("ui_up"):
+		position.y = position.y - 10
+	if Input.is_action_pressed("ui_down"):
+		position.y = position.y - -10
